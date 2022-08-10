0c352c14288fe584e57a29e5cbf25414586e66f8
Michael Kagel <michael.kagel@web.de>
2022-08-10 20:28:54 +0200

    Initial commit

---
+++ src/main/java/de/codelake/unpgraphql/UnPSpringBootGraphQlApplication.java
@@ -0,0 +1,13 @@
+package de.codelake.unpgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnPSpringBootGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnPSpringBootGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/codelake/unpgraphql/UnPSpringBootGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package de.codelake.unpgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnPSpringBootGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
